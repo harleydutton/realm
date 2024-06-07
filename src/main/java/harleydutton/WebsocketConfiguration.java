117741a2264dfb318a38a349d72871fb72fa4e08
Harley Dutton <harleydutton@gmail.com>
2024-06-07 14:27:31 -0600

    turned on autosave:

---
+++ src/main/java/harleydutton/WebsocketConfiguration.java
@@ -0,0 +1,15 @@
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/sow").withSockJS();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+}
