a3f485ab969f0613fe4ec730acaf594ca6742ee9
Harley Dutton <harleydutton@gmail.com>
2024-06-07 14:58:27 -0600

    maven has finally settled the fuck down

--- src/main/java/harleydutton/Main.java
+++ src/main/java/harleydutton/Main.java
@@ -3,5 +3,10 @@ package harleydutton;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //gameplan
+        //main thread listens for connections
+        //connections need a username and password
+        //
     }
 }
--- src/main/java/harleydutton/WebsocketConfiguration.java
+++ src/main/java/harleydutton/WebsocketConfiguration.java
@@ -1,15 +1,14 @@
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+//     @Override
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         registry.addEndpoint("/sow").withSockJS();
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/sow").withSockJS();
+//     }
 
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
-    }
-}
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry registry) {
+//         registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+//     }
+// }
