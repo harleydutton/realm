e87b9339cf22fc0eba2af59a9b54a67a24f581e5
Harley Dutton <harleydutton@gmail.com>
2024-07-25 02:01:02 -0600

    flattened player out a bit. not sure if this is good

--- Scenes/Marker/Marker.gd
+++ Scenes/Marker/Marker.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var cam = $"../CameraRig/SpringArm3D/Camera3D"
 @onready var page = $"../Page"
+
 func _input(_event):
 	if Input.is_action_pressed("click"):
 		var intersection = cam.get_camera_collision()
--- Scenes/Page/Page.gd
+++ Scenes/Page/Page.gd
@@ -3,6 +3,9 @@ extends Node3D
 var voxel = preload("res://Scenes/Page/Voxel.tscn")
 var voxels = []
 
+func _ready():
+	add(Vector3(0,0,0))
+
 func add(pos):
 	var instance = voxel.instantiate()
 	instance.position = pos
